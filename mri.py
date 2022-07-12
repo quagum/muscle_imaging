bf4598020e7651be065f49dd5cd4b02956966c91
Charles Wang <quagum125@gmail.com>
2022-07-12 12:36:45 -0400

    generation of 3d points

--- mri.py
+++ mri.py
@@ -1,10 +1,12 @@
 from concurrent.futures import process
+from inspect import CORO_RUNNING
 import cv2 as cv
 from cv2 import ellipse
 from cv2 import split
 from cv2 import CAP_PROP_SPEED
 from matplotlib.pyplot import draw, gray
 import numpy as np
+from stl import mesh 
 import os
 
 def convert_64_8(image_64):
@@ -30,15 +32,18 @@ def image_processing(input_image):
     convert = convert_64_8(processed)
     return convert
 
-def draw_blank_lines(input_image, canvas):
+def draw_blank_lines(input_image, canvas, z):
     minLineLength = 150
     maxLineGap = 0
     lines = cv.HoughLinesP(input_image, cv.HOUGH_PROBABILISTIC, np.pi/180, 200, maxLineGap, minLineLength)
+    coordinates = []
     for x in range(0, len(lines)):
         for x1,y1,x2,y2 in lines[x]:
+            line = [[x1, y1, z], [x2, y2, z]]
             pts = np.array([[x1, y1 ], [x2 , y2]], np.int32)
-            cv.polylines(canvas, [pts], True, (0, 255, 255), 3)
-    return canvas
+            coordinates.append(line)
+            cv.polylines(canvas, [pts], True, (0, 255, 255), 1)
+    return canvas, coordinates
     
 #x=476 y=200
 #not all MRI scans will have the same middle --> how to find the middle?  
@@ -53,7 +58,7 @@ def automatic_crop(input_image):
     crop = input_image[min_y:max_y,min_x:max_x]
     return crop 
 
-def multi(): 
+def multi_image(): 
     dir = 'MRI'
     for file in os.listdir(dir):
         os.chdir(r'C:\Users\charl\njit\muscle_imaging')
@@ -78,21 +83,36 @@ def multi():
         except:
             print("error")
 
-
 def single():
     image = cv.imread(r"C:\Users\charl\njit\muscle_imaging\scans\cIMG-0007-00001.jpg")
+    z = 1
     cropped = image[100:600, 0:951]
     scaled = cv.pyrDown(cropped)  
     gray = cv.cvtColor(scaled, cv.COLOR_BGR2GRAY)
     sobel = cv.Sobel(gray, cv.CV_64F, dx=0, dy=1, ksize=3)
-    cv.imshow('sobel', sobel)
     blurred = cv.GaussianBlur(sobel, (31, 31), 0, 1)
-    cv.imshow('sobel blur', blurred)
     T, thresh = cv.threshold(blurred, 1, 255, cv.THRESH_BINARY)
-    cv.imshow('thresh', thresh)
 
     convert = convert_64_8(thresh)
-    cv.imshow('final', draw_blank_lines(convert, scaled))
+    final, coordinates = draw_blank_lines(convert, scaled, z)
+    print(coordinates)
+    cv.imshow('final', final)
     cv.waitKey(0)
 
-multi()
+def multi_point():
+    dir = 'MRI'
+    for file in os.listdir(dir):
+        z = int(file[10:len(file)-4])
+        f = os.path.join(dir, file)
+        image = cv.imread(f)
+        cropped = image[100:600, 0:951]
+        scaled = cv.pyrDown(cropped)  
+        gray = cv.cvtColor(scaled, cv.COLOR_BGR2GRAY)
+        sobel = cv.Sobel(gray, cv.CV_64F, dx=0, dy=1, ksize=3)
+        blurred = cv.GaussianBlur(sobel, (31, 31), 0, 1)
+        T, thresh = cv.threshold(blurred, 1, 255, cv.THRESH_BINARY)
+        convert = convert_64_8(thresh)
+        final, coordinates = draw_blank_lines(convert, scaled, z)
+        print(coordinates)
+
+single()
